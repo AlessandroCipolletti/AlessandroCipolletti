9ecf192565e4d14832a1be92005f86816a410bf6
Alessandro Cipolletti <cipolletti.alessandro@gmail.com>
2022-05-28 01:34:08 +0200

    MySelf code - 0.5

---
+++ myself.js
@@ -0,0 +1,113 @@
+class MySelf extends FrontendEngineer {
+
+  aboutMe: '', // <-- todo
+
+  firstName: 'Alessandro',
+  lastName: 'Cipolletti',
+  age: 32,
+  from: 'Italy',
+  worksSince: 2009,
+  
+  mail: 'cipolletti.alessandro@gmail.com',
+  linkedin: 'https://www.linkedin.com/in/alessandro-cipolletti/'
+  github: 'https://github.com/AlessandroCipolletti',
+
+  workingOn: 'https://drawith.me',
+  myOwnNpmPackage: 'https://www.npmjs.com/package/js-math-and-ui-utils',
+  articles: 'https://dev.to/alessandrocipolletti',
+  videos: 'https://www.youtube.com/channel/UCgeayDnKaNFmhM4InFLIwzQ',
+
+  skills: {
+    Javascript: '⭐️⭐️⭐️⭐️⭐️',
+    Html5:      '⭐️⭐️⭐️⭐️⭐️',
+    Css3:       '⭐️⭐️⭐️⭐️⭐️',
+    Git:        '⭐️⭐️⭐️⭐️⭐️',
+    MySql:      '⭐️⭐️⭐️⭐️⭐️',
+    React:      '⭐️⭐️⭐️⭐️',
+    Redux:      '⭐️⭐️⭐️⭐️',
+    NodeJs:     '⭐️⭐️⭐️⭐️',
+    MongoDb:    '⭐️⭐️⭐️⭐️',
+    SocketIo:   '⭐️⭐️⭐️⭐️',
+    Less:       '⭐️⭐️⭐️⭐️',
+    Typescript: '⭐️⭐️⭐️',
+  },
+
+  languages: {
+    Italian: '⭐️⭐️⭐️⭐️⭐️',
+    French:  '⭐️⭐️⭐️⭐️',
+    English: '⭐️⭐️⭐️',
+  },
+  
+  livedIn: {
+    Italy: 'until 2013',    // until 23 yeras old
+    Paris: '2013...2017',   // from 23 to 28
+    DigitalNomad: '2018',   // 29
+    Bruxelles: 'from 2019', // until now
+  },
+  
+  relevantExperiences: {
+    DrawithMe: `
+      Startup  -  07.2021 / 06.2022  -  VanillaJs, NodeJs, Socket.io, AWS, Git
+      
+      My startup Drawith.Me - A mobile first web app to draw online with a friend in real time.
+      I founded Drawith.Me because I'm fascinated by what people can do when they work together,
+      and I love building complex yet performing UIs that feel natural to the user.
+
+      - I build the whole platform myself, focused on real-time performances
+      - +300K users used our platform in one year
+      - +30M views on Tiktok
+      - We won 25K private founding
+    `,
+    Pitchy: `
+      Front-End Team Lead  -  05.2017 / 06.2021  -  React, Redux, IndexedDb, NodeJs, FFMpeg, AWS, Git
+
+      Parisian startup - Saas platform to do video editing online 
+      When I joined this startup we were 15 in total, with just 3 developers, and I was the only front-end dev.
+      By the end of my experience we were 60 in total with 14 developers, and I became the front-end team lead.
+
+      - Code guidelines and code review for the team
+      - Video-editing UI with React and StyledComponents
+      - Local file system, animations, exporting with Puppeteer and FFMpeg
+    `,
+    MaxMara: `
+      iPad Manager  -  03.2012 / 05.2013  -  Vodafone MDM Manager, Sencha Touch
+
+      - Ho fatto la web app
+      - Ho configurato tutti gli ipad
+      - Insegnavo alle gerenti come usarlo
+    `,
+    AppStore: `
+      I published a few web app on the App Store  -  Occasionally between 2010 and 2013
+
+      I was fascinated by the multi touch on the new iPad, I built a few web app / small games,
+      incapsulated them inside an app container, and published them on the app store.
+
+      - Learned ho apple store works
+      - Earned a few thousand euros from an hobby
+    `,
+    ZenSistemi: `
+      Software Developer  -  10.2009 / 03.2012  -  Visual Basic, Microsoft Access, Sql Server, PHP
+
+        My first job right after high school.
+        I did a lot of SQL queries, Visual Basic, and customer care.
+
+      - Database manager on Microsoft SqlServer
+      - Customers service for our users
+      - The main project was a CRM built internally, based on Microsoft Access
+      - Worked on the internal tickets web platform in PHP
+    `,
+    LavorettoOccasionale: `
+      insegnare informatica ai vecchietti
+    `,
+  },
+
+  formazione: {
+    SempreStudiatoPerCazziMiei: {
+
+    },
+    highSchool: {
+
+    },
+  },
+
+}'
